755f0769995e1bb58e5bcae33c04ff0cc5c83b28
FeedMyKids1 <47508141+KampinKarl1@users.noreply.github.com>
2023-03-19 11:48:30 +0100

    Increased number of names (with help from ChatGPT)
    Prompted ChatGPT to "Generate random names with Germanic, Irish, Spanish, French, and Polish roots. Let's throw in a few eastern Asian names and maybe one or two Samoan/Hawaiian names as well."
    
    Also added a method for Spanish format names => First name + mother and father's last names.

--- Names.cs
+++ Names.cs
@@ -6,7 +6,13 @@ public static class Names
         "Bob", "Bill", "Beatrice", "Betty", "Barbara",
         "Charles", "Chelsea", "Chandra", "Chester", "Corey",
         "Dave", "Donald", "Dina", "Doris",
-        "Evan", "Emma"
+        "Evan", "Emma",  "Liam", "Noah", "Oliver", "Ethan", "Aiden", "Lucas", "Mason", "Logan", "Caleb", "Elijah",
+        "Declan", "Finn", "Liam", "Ronan", "Seamus", "Ciaran", "Aisling", "Bridget", "Saoirse", "Niamh",
+        "Diego", "Alejandro", "Carlos", "Santiago", "Isabella", "Sofia", "Juan", "Miguel", "Emilia", "Valeria",
+        "Pierre", "Lucien", "Gaston", "Aurelie", "Camille", "Julien", "Claire", "Sophie", "Antoine", "Gabriel",
+        "Mateusz", "Jakub", "Kacper", "Lena", "Alicja", "Oskar", "Karol", "Maja", "Julia", "Kuba",
+        "Haruka", "Aiko", "Yuki", "Akira", "Kenji", "Hana", "Sora", "Kai", "Jin", "Naoki",
+        "Alofa", "Lauli'i", "Pili", "Leilani", "Kai", "Kiana", "Malie", "Moana", "Nalani", "Alohi"
     };
 
     public static readonly string[] _LastNames =
@@ -19,7 +25,12 @@ public static class Names
         "Nolan",
         "Oppenheimer",
         "Torvalds", "Turing",
-        "Zhukov"
+        "Zhukov", "Smith", "Johnson", "Brown", "Jones", "Garcia", "Miller", "Davis", "Martinez", "Gonzalez", "Lopez",
+        "Schmidt", "Müller", "Schneider", "Fischer", "Weber", "Bauer", "Richter", "Schulz", "Kowalski", "Nowak",
+        "O'Reilly", "Murphy", "Kelly", "Sullivan", "O'Connor", "Gallagher", "Kennedy", "Ryan", "Fitzgerald", "Doyle",
+        "Gonzalez", "Rodriguez", "Hernandez", "Martinez", "Perez", "Sanchez", "Gomez", "Flores", "Diaz", "Reyes",
+        "Lee", "Kim", "Choi", "Park", "Sato", "Suzuki", "Yamamoto", "Tanaka", "Lam", "Nguyen",
+        "Smith", "Brown", "Johnson", "Jones", "Williams", "Davis", "Miller", "Wilson", "Moore", "Taylor"
     };
 
     static System.Random random = new System.Random();
@@ -28,4 +39,5 @@ public static class Names
     public static string RandomLastName() => _LastNames[random.Next(0, _LastNames.Length)];
 
     public static string RandomFullName() => RandomFirstName() + " " + RandomLastName();
+    public static string RandomSpanishFormatName()=> RandomFirstName() + " " + RandomLastName() + " " + RandomLastName();
 }
